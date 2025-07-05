516117ef866fa1a7c673c48fa436fa93d8761e5d
cnnr137 <connerlennon137@gmail.com>
2025-07-04 23:17:17 -0400

    they actually talk now which is cool

--- server.cpp
+++ server.cpp
@@ -1,49 +1,52 @@
-int fd = socket(AF_INET, SOCK_STREAM, 0);
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int val = 1;
-setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-
-struct sockaddr_in {
-    uint16_t sin_family; // AF_INET
-    uint16_t sin_port;   // port in big endian
-    struct in_addr sin_addr; // IPv4
-};
-
-struct in_addr {
-    uint32_t s_addr; // IPv4 in big endian
-}
-
-struct sockaddr_in addr = {};
-addr.sin_family = AF_INET;
-addr.sin_port = htons(1234); // port
-addr.sin_addr.s_addr = htonl(0); // wildcard IP 0.0.0.0
-int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-if (rv) { die("bind()"); };
-
-// listen 
-rv = listen(fd, SOMAXCONN);
-if (rv) { die("listen()") };
-
-while (true) {
-    //accept
-    struct sockaddr_in client_addr = {};
-    socklen_t addrlen = sizeof(client_addr);
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-    if (connfd < 0) {
-        continue; // error
-    } 
-    do_something(connfd);
-    close(connfd);
+void die(const char *msg) {
+    perror(msg);
+    exit(1);
 }
 
 static void do_something(int connfd) {
     char rbuf[64] = {};
     ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
     if (n < 0 ) {
-        msg("read() error");
+        die("read() error");
         return;
     }
     printf("client says: %s\n", rbuf);
     char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf))
+    write(connfd, wbuf, strlen(wbuf));
+}
+
+int main() 
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(0);
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    while (true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0) {
+            continue;
+        }
+        do_something(connfd);
+        close(connfd);
+    }
 }
